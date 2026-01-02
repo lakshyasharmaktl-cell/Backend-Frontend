25ea3965df479006c6f1e3cfa92554f346b4dce6
Lakshaya <lakshyasharmaktl@gmail.com>
2026-01-02 16:55:13 +0530

    updated

---
+++ backend/src/controller/user_controller.js
@@ -0,0 +1,29 @@
+import user_model from '../models/user_models.js'
+
+export const create_user = async (req, res) => {
+    try {
+        const data = req.body
+
+        const { email } = data
+
+        const randomOtp = Math.floor(1000 + Math.random() * 9000)
+        const expiryTime = Date.now() + 5 * 60 * 1000;
+
+        const checkUser = await user_model.findOneAndUpdate({ email: email },
+            { $set: { 'user.userOtp': randomOtp, 'user.otpExpire': expiryTime } })
+
+            if (checkUser) {
+            const { isVerify, isDelete } = checkUser.user
+
+            if (isDelete) return res.status(200).send({ status: true, msg: "Your Account is Delete" })
+            if (!isVerify) return res.status(200).send({ status: true, msg: "resend otp send ..." })
+            if (isVerify) return res.status(200).send({ status: true, msg: "accosunt already verify pls login..." })
+
+        }
+
+        const DB = await user_model.create(data)
+
+        return res.status(201).send({ status: true, msg: "SucessFull Create User", DB })
+    }
+    catch (err) { return res.status(500).send({ status: false, msg: err.message }) }
+}  
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Signup from './Components/signup/Signup.jsx'
+
+export default function App() {
+  return (
+    <div>
+      <Signup />
+
+    </div>
+  )
+}
